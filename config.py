baab8521e97c50f424da0241b96684f1585e680d
CvaTricks <68840422+cvatricks@users.noreply.github.com>
2026-03-04 17:50:10 +0530

    Modify database name and collections in config.py
    Updated database name and collection references in Config class.

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ class Config(object):
     # MONGODB_URI = "mongodb+srv://sandytpm5959_db_user:ugTrbPHLOhpGE79F@tracebility.y637mls.mongodb.net/?retryWrites=true&w=majority&appName=Tracebility"
     MONGODB_URI = "mongodb+srv://sivaprakasamulaganathan:7yVnXJwsk8M1p1Af@cluster0.v6vqw1o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     myclient = pymongo.MongoClient(MONGODB_URI)
-    mydb = myclient["mydatabase"]
+    mydb = myclient["astro"]
     users = mydb["users"]
     sessions = mydb["sessions"]
-    workdata = mydb["data"]
-    jobid = mydb["jobid"]
+    workdata = mydb["db"]
 
     # Connection
     HOST = "traceability-sdzt.onrender.com"
     IP = "0.0.0.0"
     PORT = "80"
+
 
 
