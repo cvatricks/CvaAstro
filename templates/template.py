a2b23f27b82dc6465882c2cb968663fd7b817e6d
CvaTricks <68840422+cvatricks@users.noreply.github.com>
2026-03-15 22:55:15 +0530

    Refactor HTML table structure in template.py

--- templates/template.py
+++ templates/template.py
@@ -1,86 +1,69 @@
 class Template(object):
 
-    report = """
-<table border="1" style="width: 100%; max-width: 360px; text-align: center; 
-
-border-collapse: collapse; background-color: #FFF6E6; font-family: Arial, sans-
-
-serif; font-weight: bold; font-color: #FFB752; border-color: #FF852E;">
+report = """
+<table border="1"
+    style="width: 100%; max-width: 360px; border-collapse: collapse; background-color: #FFF6E6; font-family: Arial, sans-serif; font-weight: bold; font-color: #FFB752; border-color: #FF852E;">
     <tr>
-        <td style="width: 25%; height: 75px;"><p style="color:brown;font-
-
-size:12px;text-align:left;">1<br><br><br><br></p><p style="color:brown;font-
-
-size:10px;text-align:right;">மீனம்</p></td>
-        <td style="width: 25%;"><p style="color:brown;font-size:12px;text-
-
-align:left;">2<br><br><br><br></p><p style="color:brown;font-size:10px;text-
-
-align:right;">மேஷம்</p></td>
-        <td style="width: 25%;"><p style="color:brown;font-size:12px;text-
-
-align:left;">3<br><br><br><br></p><p style="color:brown;font-size:10px;text-
-
-align:right;">ரிஷப</p></td>
-        <td style="width: 25%;"><p style="color:brown;font-size:12px;text-
-
-align:left;">4<br><br><br><br></p><p style="color:brown;font-size:10px;text-
-
-align:right;">மிதுன</p></td>
+        <td style="width: 25%; height: 75px;">
+            <p style="color:brown;font-size:12px;text-align:left;">1<br><br><br><br></p>
+            <p style="color:brown;font-size:10px;text-align:right;">மீனம்</p>
+        </td>
+        <td style="width: 25%;">
+            <p style="color:brown;font-size:12px;text-align:left;">2<br><br><br><br></p>
+            <p style="color:brown;font-size:10px;text-align:right;">மேஷம்</p>
+        </td>
+        <td style="width: 25%;">
+            <p style="color:brown;font-size:12px;text-align:left;">3<br><br><br><br></p>
+            <p style="color:brown;font-size:10px;text-align:right;">ரிஷப</p>
+        </td>
+        <td style="width: 25%;">
+            <p style="color:brown;font-size:12px;text-align:left;">4<br><br><br><br></p>
+            <p style="color:brown;font-size:10px;text-align:right;">மிதுன</p>
+        </td>
     </tr>
-    
+
     <tr>
-        <td style="height: 75px;"><p style="color:brown;font-size:12px;text-
-
-align:left;">5<br><br><br><br></p><p style="color:brown;font-size:10px;text-
-
-align:right;">கும்ப</p></td>
-        <td colspan="2" rowspan="2" style="background-color: #ffffff; font-size: 
-
-16px;">
+        <td style="height: 75px;">
+            <p style="color:brown;font-size:12px;text-align:left;">5<br><br><br><br></p>
+            <p style="color:brown;font-size:10px;text-align:right;">கும்ப</p>
+        </td>
+        <td colspan="2" rowspan="2" style="background-color: #ffffff; font-size: 16px; text-align: center;">
             ராசிக் கட்டம்<br>(Rasi Chart)
         </td>
-        <td><p style="color:brown;font-size:12px;text-
-
-align:left;">6<br><br><br><br></p><p style="color:brown;font-size:10px;text-
-
-align:right;">கடக</p></td>
+        <td>
+            <p style="color:brown;font-size:12px;text-align:left;">6<br><br><br><br></p>
+            <p style="color:brown;font-size:10px;text-align:right;">கடக</p>
+        </td>
     </tr>
-    
+
     <tr>
-        <td style="height: 75px;"><p style="color:brown;font-size:12px;text-
-
-align:left;">7<br><br><br><br></p><p style="color:brown;font-size:10px;text-
-
-align:right;">மகர</p></td>
-        <td><p style="color:brown;font-size:12px;text-
-
-align:left;">8<br><br><br><br></p><p style="color:brown;font-size:10px;text-
-
-align:right;">சிம்ம</p></td>
+        <td style="height: 75px;">
+            <p style="color:brown;font-size:12px;text-align:left;">7<br><br><br><br></p>
+            <p style="color:brown;font-size:10px;text-align:right;">மகர</p>
+        </td>
+        <td>
+            <p style="color:brown;font-size:12px;text-align:left;">8<br><br><br><br></p>
+            <p style="color:brown;font-size:10px;text-align:right;">சிம்ம</p>
+        </td>
     </tr>
-    
+
     <tr>
-        <td style="height: 75px;"><p style="color:brown;font-size:12px;text-
-
-align:left;">9<br><br><br><br></p><p style="color:brown;font-size:10px;text-
-
-align:right;">தனுசு</p></td>
-        <td><p style="color:brown;font-size:12px;text-
-
-align:left;">10<br><br><br><br></p><p style="color:brown;font-size:10px;text-
-
-align:right;">விருச்</p></td>
-        <td><p style="color:brown;font-size:12px;text-
-
-align:left;">11<br><br><br><br></p><p style="color:brown;font-size:10px;text-
-
-align:right;">துலாம்</p></td>
-        <td><p style="color:brown;font-size:12px;text-
-
-align:left;">12<br><br><br><br></p><p style="color:brown;font-size:10px;text-
-
-align:right;">கன்னி</p></td>
+        <td style="height: 75px;">
+            <p style="color:brown;font-size:12px;text-align:left;">9<br><br><br><br></p>
+            <p style="color:brown;font-size:10px;text-align:right;">தனுசு</p>
+        </td>
+        <td>
+            <p style="color:brown;font-size:12px;text-align:left;">10<br><br><br><br></p>
+            <p style="color:brown;font-size:10px;text-align:right;">விருச்</p>
+        </td>
+        <td>
+            <p style="color:brown;font-size:12px;text-align:left;">11<br><br><br><br></p>
+            <p style="color:brown;font-size:10px;text-align:right;">துலாம்</p>
+        </td>
+        <td>
+            <p style="color:brown;font-size:12px;text-align:left;">12<br><br><br><br></p>
+            <p style="color:brown;font-size:10px;text-align:right;">கன்னி</p>
+        </td>
     </tr>
 </table>
 
